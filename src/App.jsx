474f1cd56b20a39f45a1f042327dd5c6865d2d3f
Simeon Griggs <simeon@tomoro.com.au>
2020-12-08 18:01:21 +0000

    add responsive versions

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
 import React, { useState } from 'react'
 
 function App() {
+  const cols = {
+    one: <ColOne />,
+    two: <ColTwo />,
+    three: <ColThree />,
+  }
+
+  const colsDisplay = ['one', 'two', 'one', 'two', 'one', 'three']
+
   return (
-    <div className="flex font-black text-3xl h-screen">
-      <div className="w-1/7 bg-pink-100 text-pink-700 h-full flex items-center justify-center">
-        <div className="transform rotate-90">
-          1/7
-        </div>
+    <div className="flex font-black text-2xl h-screen">
+      {colsDisplay.map((col, index) => (
+        <React.Fragment key={index}>{cols[col]}</React.Fragment>
+      ))}
+    </div>
+  )
+}
+
+function ColOne() {
+  return (
+    <div className="w-1/7 lg:w-1/16 bg-pink-100 text-pink-700 h-full flex items-center justify-center">
+      <div className="transform rotate-90">
+        <span className="lg:hidden">1/7</span>
+        <span className="hidden lg:inline">lg:w-1/16</span>
       </div>
-      <div className="w-2/7 border-r border-l border-blue-200 bg-blue-100 text-blue-700 h-full flex items-center justify-center">
-        <div className="transform rotate-90">
-          2/7
-        </div>
+    </div>
+  )
+}
+
+function ColTwo() {
+  return (
+    <div className="w-2/7 lg:w-3/16 bg-blue-100 text-blue-700 h-full flex items-center justify-center">
+      <div className="transform rotate-90">
+        <span className="lg:hidden">2/7</span>
+        <span className="hidden lg:inline">lg:w-3/16</span>
       </div>
-      <div className="w-1/7 bg-pink-100 text-pink-700 h-full flex items-center justify-center">
-        <div className="transform rotate-90">
-          1/7
-        </div>
-      </div>
-      <div className="w-2/7 border-r border-l border-blue-200 bg-blue-100 text-blue-700 h-full flex items-center justify-center">
-        <div className="transform rotate-90">
-          2/7
-        </div>
-      </div>
-      <div className="w-1/7 bg-pink-100 text-pink-700 h-full flex items-center justify-center">
-        <div className="transform rotate-90">
-          1/7
-        </div>
+    </div>
+  )
+}
+
+function ColThree() {
+  return (
+    <div className="w-2/7 lg:w-7/16 bg-green-100 text-green-700 h-full hidden lg:flex items-center justify-center">
+      <div className="transform rotate-90">
+        <span className="lg:hidden">2/7</span>
+        <span className="hidden lg:inline">lg:w-7/16</span>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 const fractionWidths = require('./index')
 
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
   purge: [],
   theme: {},
   variants: {},
-  plugins: [
-    fractionWidths(16),
-    fractionWidths([2,7]),
-  ],
+  plugins: [fractionWidths(16), fractionWidths([2, 7])],
 }
